2dccf5b2cded49a28e1dc32c084c01d2e3a4ad87
Devan019 <devanchauhan012@gmail.com>
2024-12-22 22:26:55 +0530

    some

--- src/provider/SocketProvider.js
+++ src/provider/SocketProvider.js
@@ -1,20 +1,23 @@
 import { createContext, useContext, useMemo } from 'react'
 
-import {io} from 'socket.io-client'
+import { io } from 'socket.io-client'
 
 const SocketContext = createContext();
 
-export const useSocket = ()=>{
-    return useContext(SocketContext)
+export const useSocket = () => {
+  return useContext(SocketContext)
 }
 
-export const SocketProvider = ({children}) => {
-    const socketio = useMemo(()=>{
-       return io('https://omegal-backend-4j09.onrender.com')
-    } , [])
+export const SocketProvider = ({ children }) => {
+  const socketio = useMemo(() => {
+    return io('wss://omegal-backend-4j09.onrender.com', {
+      transports: ["websocket"],
+      secure: true,
+    })
+  }, [])
   return (
     <SocketContext.Provider value={socketio}>
-        {children}
+      {children}
     </SocketContext.Provider>
   )
 }
